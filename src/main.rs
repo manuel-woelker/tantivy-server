7200a80d1708ab86dc74ad19558df018fa64a68d
Manuel Woelker <github@manuel.woelker.org>
2017-01-27 21:21:33 +0100

    feat(UI): Basic Swagger UI

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate iron;
 extern crate mount;
 extern crate staticfile;
+extern crate hyper;
 
 use iron::prelude::*;
 use iron::status;
 use std::path::Path;
+
+use iron::headers::{Headers, ContentType};
+use iron::modifiers::Header;
+use iron::mime::{Mime, TopLevel, SubLevel, Attr, Value};
 
 use mount::Mount;
 use staticfile::Static;
@@ -20,6 +25,17 @@ fn main() {
     });
     // Serve the static file docs at /doc/
     mount.mount("/swagger/", Static::new(Path::new("docs/api/api.swagger.yaml")));
+    mount.mount("/docs/swagger/", Static::new(Path::new("assets/swagger-ui/")));
+    mount.mount("/api/status", |_: &mut Request| {
+        let mut headers = Headers::new();
+
+        headers.set(
+            ContentType(Mime(TopLevel::Application, SubLevel::Json,
+                             vec![(Attr::Charset, Value::Utf8)]))
+        );
+        Ok(Response::with((status::Ok, Header(ContentType(Mime(TopLevel::Application, SubLevel::Json,
+                                                         vec![(Attr::Charset, Value::Utf8)]))), format!("{{\"status\": \"green\", \"version\": \"{}\"}}", env!("CARGO_PKG_VERSION")))))
+    });
 
     let listening = Iron::new(mount).http("localhost:3000");
     let listening = listening.unwrap();
